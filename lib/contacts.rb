f0c1284b5b8e7b2d5c347b7800c883f62cfec12e
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-28 13:59:12 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,16 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each { |person, contact_detail_hash|
+    if person == "Freddy Mercury"
+      contact_detail_hash.each { |attribute, data|
+        if attribute == :favorite_ice_cream_flavors 
+          data.delete_if { |ice_cream| 
+            ice_cream == "strawberry"
+          }
+        
+        end 
+      }
+    end
+  }
 end
